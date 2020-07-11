3ef21200a2f4a76ca72f3b03960327ef8c365489
Mehdi Khoshnevisz <mkhnsz1996@gmail.com>
2020-07-11 22:38:58 +0430

    feat: add splash component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Splash />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Splash from '@/components/Splash';
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Splash
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/Splash.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="vue-splash">
+    <div class="text-center">
+      <div class="vue-splash__anim mb-3">
+        <img src="@/assets/vue.svg" />
+      </div>
+      <div class="vue-splash__text">
+        <span>Splash Your Logo</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Splash"
+};
+</script>
+
+<style scoped>
+.vue-splash {
+  top: 0;
+  left: 0;
+  flex: 1;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  position: fixed;
+  align-items: center;
+  justify-content: center;
+}
+
+.vue-splash__anim {
+  animation: loadingAnimation 1.3s infinite;
+}
+
+.vue-splash__anim img {
+  max-width: 230px;
+}
+
+.vue-splash__text {
+  margin-top: 60px;
+  font-size: 20px;
+}
+
+@keyframes loadingAnimation {
+  0% {
+    filter: drop-shadow(0px 0px 0px #00BFA5);
+  }
+  100% {
+    filter: drop-shadow(0px 0px 200px #00BFA5);
+  }
+}
+</style>
