9e31889b2721441ef07d0b8d6532d51bab8c7cf5
Mehdi Khoshnevisz <mkhnsz1996@gmail.com>
2020-07-29 22:12:27 +0430

    feat: Create ForkMe component

---
+++ src/components/ForkMe.vue
@@ -0,0 +1,20 @@
+<template>
+  <a id="fork-me" href="https://github.com/MehdiKhoshnevisz/vue-splash">
+    <img
+      width="149"
+      height="149"
+      src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149"
+      class="attachment-full size-full"
+      alt="Fork me on GitHub"
+      data-recalc-dims="1"
+    />
+  </a>
+</template>
+
+<style lang="scss">
+  #fork-me {
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
+</style>
