84f2ef55c2ca251b322de01f3cf60596ee1b77a5
Mehdi Khoshnevisz <mkhnsz1996@gmail.com>
2020-07-30 02:26:49 +0430

    refactor: Update Browser

--- src/components/Browser.vue
+++ src/components/Browser.vue
@@ -1,35 +1,55 @@
 <template>
   <div class="vue-splash-browser">
     <div class="vue-splash-browser__header">
-      <span class="vue-splash-browser__header-action" id="action-danger"></span>
-      <span
-        class="vue-splash-browser__header-action"
-        id="action-warning"
-      ></span>
-      <span
-        class="vue-splash-browser__header-action"
-        id="action-success"
-      ></span>
-      <div
-        :class="[
-          'vue-splash-browser__loader',
-          { 'vue-splash-browser__loader--end': completeLoader },
-        ]"
-        :style="loaderStyle"
-      ></div>
+      <div class="vue-splash-browser__header__actions">
+        <span
+          class="vue-splash-browser__header-action"
+          id="action-danger"
+        ></span>
+        <span
+          class="vue-splash-browser__header-action"
+          id="action-warning"
+        ></span>
+        <span
+          class="vue-splash-browser__header-action"
+          id="action-success"
+        ></span>
+
+        <transition name="fade">
+          <div
+            v-if="!completeLoader"
+            class="vue-splash-browser__loader"
+            :style="loaderStyle"
+          ></div>
+        </transition>
+      </div>
+
+      <div class="vue-splash-browser__header__buttons">
+        <img
+          class="vue-splash-browser__header-button"
+          src="@/assets/images/reload.svg"
+          @click="reloadBrowser"
+        />
+      </div>
     </div>
 
-    <div v-if="!isLoaded" class="vue-splash-browser__splash-wrapper">
-      <slot></slot>
-    </div>
+    <transition name="fade">
+      <div v-if="isLoaded" class="vue-splash-browser__splash-wrapper">
+        <Splash :logo="logo" />
+      </div>
+    </transition>
 
-    <div v-else class="vue-splash-browser__content">
-      <span>browser content</span>
-    </div>
+    <transition name="fade">
+      <div v-if="!isLoaded" class="vue-splash-browser__content">
+        <span>browser content</span>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
+import VueLogo from "@/assets/vue.svg";
+
 export default {
   name: "Browser",
   data() {
@@ -48,25 +68,37 @@ export default {
         width: this.loaderWidth + "%",
       };
     },
+    logo() {
+      return VueLogo;
+    },
   },
   methods: {
     startTimeout() {
+      this.isLoaded = true;
+
       let miliSeconds = 4000;
       let intervalMiliSeconds = miliSeconds / 100;
 
       setTimeout(() => {
-        this.isLoaded = true;
+        this.isLoaded = false;
       }, miliSeconds);
 
       setInterval(() => {
-        if (this.isLoaded) {
+        if (!this.isLoaded) {
           this.loaderWidth = 100;
-          this.completeLoader = true;
+
+          setTimeout(() => {
+            this.completeLoader = true;
+          }, 0);
+
           return;
         }
 
         this.loaderWidth++;
       }, intervalMiliSeconds);
+    },
+    reloadBrowser() {
+      this.$emit("update");
     },
   },
 };
@@ -82,10 +114,16 @@ export default {
   box-shadow: 0 0.5rem 2rem rgba(0, 0, 0, 0.2);
 
   &__header {
-    display: flex;
     padding: 1rem;
+    display: flex;
     position: relative;
+    align-items: center;
     background-color: #f3f3f3;
+    justify-content: space-between;
+
+    &__actions {
+      display: flex;
+    }
 
     &-action {
       width: 0.8rem;
@@ -105,23 +143,27 @@ export default {
     #action-success {
       background-color: #28c940;
     }
+
+    &-button {
+      width: 1.2rem;
+      cursor: pointer;
+      transition: all 0.3s ease;
+
+      &:active {
+        transform: scale(0.8);
+      }
+    }
   }
 
   &__loader {
     left: 0;
     right: 0;
     width: 0;
-    opacity: 1;
     height: 2px;
     bottom: -1px;
     position: absolute;
     background: #41b883;
-    transition: all 0.3s ease;
     box-shadow: 0 0 10px #41b883;
-
-    &--end {
-      opacity: 0;
-    }
   }
 
   &__splash-wrapper {
