bb68a4fdd45cf1d0001ac8c52f7e441c03c35796
Mehdi Khoshnevisz <mkhnsz1996@gmail.com>
2020-07-30 04:00:08 +0430

    refactor: Update splashPlugin

--- src/plugin/splashPlugin.js
+++ src/plugin/splashPlugin.js
@@ -1,8 +1,27 @@
 import Splash from "@/components/Splash";
 
-export default {
-  install(Vue, options) {
-    console.log(options);
-    Vue.component("Splash", Splash);
-  }
+export function install(Vue) {
+  if (install.installed) return;
+
+  install.installed = true;
+  
+  Vue.component("Splash", Splash);
+}
+
+const plugin = {
+  install,
 };
+
+let GlobalVue = null;
+
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.Vue;
+}
+
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+export default plugin.install;
