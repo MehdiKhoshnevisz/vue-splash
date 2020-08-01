0dc23890a6cb8a8d5c9b7151abe9f6e1ac7827a7
Mehdi Khoshnevisz <mkhnsz1996@gmail.com>
2020-08-02 00:31:32 +0430

    chore: Add terser-webpack-plugin for optimization

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,35 @@
+const TerserPlugin = require("terser-webpack-plugin");
+
 module.exports = {
-  publicPath: '',
+  publicPath: "",
+
   css: {
-    extract: false
+    extract: false,
+  },
+
+  configureWebpack: config => {
+    config.optimization = {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            ecma: undefined,
+            warnings: false,
+            parallel: true,
+            parse: {},
+            compress: { drop_console: true },
+            mangle: true, // Note `mangle.properties` is `false` by default.
+            module: false,
+            output: { comments: false },
+            toplevel: false,
+            nameCache: null,
+            ie8: false,
+            keep_classnames: undefined,
+            keep_fnames: false,
+            safari10: false,
+          },
+        }),
+      ],
+    }
   }
-}
+};
